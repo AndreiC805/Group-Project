0e96137d51e47378f0dd60600b095a651417aeb7
AndreiC805 <condria.razvan@gmail.com>
2025-11-08 11:44:50 +0000

    Classes defined

--- src/class.kt
+++ src/class.kt
@@ -1,2 +1,21 @@
-class `class` {
+class Destination(val name: String, val singleFare: Double, val returnFare: Double) {
+    var totalTakings: Double = 0.0
+}
+
+class Ticket(
+    val origin: String,
+    val destination: String,
+    val ticketType: String, // "Single" or "Return"
+    val price: Double
+) {
+
+    class TicketMachine(val originStation: String) {
+    }
+    private val destinations = listOf(
+        Destination("London", 25.00, 45.00),
+        Destination("Manchester", 30.00, 55.00),
+        Destination("Birmingham", 20.00, 35.00),
+        Destination("Liverpool", 28.00, 50.00),
+        Destination("Leeds", 27.50, 49.00)
+    )
 }
